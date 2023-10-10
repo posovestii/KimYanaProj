b2533532bcb3c368ab07460a07b3281062f59254
posovestii <kimyana2007@gmail.com>
2023-10-10 19:20:44 +0300

    commit

--- PZ_2/PZ_2.py
+++ PZ_2/PZ_2.py
@@ -1,4 +1,13 @@
-num = int(input("Введите трехзначное число: "))
+# ввод трехзначного числа и вывод его средней и последней цифры
+while True:  # обработка исключений
+    try:
+        num = int(input("Введите трехзначное число: "))
+        if num <= 99 or num >= 1000:
+            print("Ошибка! Вы ввели не трехзначное число!")
+            continue
+        break
+    except ValueError:
+        print("Вы ввели не число!")
 last_digit = num % 10
 second_digit = num//10 % 10
 print("Последняя цифра:", last_digit)
