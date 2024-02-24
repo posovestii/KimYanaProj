2f2b3a3bb0125dcdc4d472420086164b8b92d000
posovestii <kimyana2007@gmail.com>
2024-02-24 11:54:29 +0300

    commit pz_12 edit

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -4,15 +4,14 @@
 # 3. произведение элементов
 
 import random
+from functools import reduce
 
 n = 10
 num = [random.randint(-100, 100) for _ in range(n)]
 max_positive = max(filter(lambda x: x > 0, num), default=None)
 min_negative = min(filter(lambda x: x < 0, num), default=None)
 
-prod = 1
-for elem in num:
-    prod *= elem
+prod = reduce(lambda x, y: x * y, num, 1)
 
 print("Последовательность чисел:", num)
 print("Максимальное положительное число:", max_positive)
