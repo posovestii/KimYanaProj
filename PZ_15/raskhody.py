f564551f606e3a7fcaf78cf8f5e28fb85cf4225d
posovestii <kimyana2007@gmail.com>
2024-04-10 12:36:12 +0300

    commit pz_15 edit3.0

--- PZ_15/raskhody.py
+++ PZ_15/raskhody.py
@@ -14,67 +14,73 @@ with sq.connect('raskhody.db') as con:
             raskh_res INTEGER NOT NULL,
             summa INTEGER NOT NULL)""")
 
-# with sq.connect('raskhody.db') as con:
-#     cur = con.cursor()
-#     con.executemany("INSERT INTO raskhody VALUES (?, ?, ?, ?, ?)", info_raskhody)
+with sq.connect('raskhody.db') as con:
+    cur = con.cursor()
+    con.executemany("INSERT INTO raskhody VALUES (?, ?, ?, ?, ?)", info_raskhody)
 
 # select
-#
+
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("SELECT imya_prod, summa FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("SELECT data_r, imya_prod FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("SELECT prod_id, raskh_res FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 
 # update
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("UPDATE raskhody SET raskh_res = 600 WHERE prod_id = 3")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("UPDATE raskhody SET raskh_res = raskh_res+400  WHERE summa < 2000")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
 # print(result)
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("UPDATE raskhody SET summa = summa-50 WHERE raskh_res > 200")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 
 # delete
 
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("DELETE FROM raskhody WHERE prod_id = 7")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("DELETE FROM raskhody WHERE summa > 5000")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
 #
 #
 # with sq.connect('raskhody.db') as con:
 #     cur = con.cursor()
 #     cur.execute("DELETE FROM raskhody WHERE data_r < '2024-04-12'")
+#     cur.execute("SELECT * FROM raskhody")
 #     result = cur.fetchall()
-# print(result)
+#     print(result)
